c88e12fdff612fff5cc96114c4c44f77c2a2306e
Jose Jair <jjarosemena@gmail.com>
2022-06-26 00:06:17 -0500

    se agrega el modulo para registro de usuarios y login del sistema

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+from . import db
 from flask import Flask
 
 def create_app():
@@ -7,11 +7,17 @@ def create_app():
 
     app.config.from_mapping(
         SECRET_KEY="mikey",
-        DATABASE_HOST=os.environ.get("Flask_DATABASE_HOST"),
-        DATABASE_PASSWORD=os.environ.get("Flask_DATABASE_PASSWORD"),
-        DATABASE_USER=os.environ.get("Flask_DATABASE_USER"),
-        DATABASE=os.environ.get("Flask_DATABASE"),
+        DATABASE_HOST=os.environ.get("FLASK_DATABASE_HOST"),
+        DATABASE_PASSWORD=os.environ.get("FLASK_DATABASE_PASSWORD"),
+        DATABASE_USER=os.environ.get("FLASK_DATABASE_USER"),
+        DATABASE=os.environ.get("FLASK_DATABASE"),
     )
+
+    db.init_app(app)
+
+    from . import auth
+
+    app.register.blueprint(auth.bp)
 
     @app.route('/hola')
     def hola():
